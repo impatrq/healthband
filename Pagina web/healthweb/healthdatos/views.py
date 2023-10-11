bee65aea805d843fb077d4708a7a2b99de4c72bf
gonza-g9673 <gonzalogomez9673@gmail.com>
2023-10-11 13:53:58 -0300

    Test Web
    Se suve una version preliminar de la pagina para poder intentar hostearla en vercell.

--- Pagina web/healthweb/healthdatos/views.py
+++ Pagina web/healthweb/healthdatos/views.py
@@ -4,3 +4,19 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def team(request):
+    return render(request, "team.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def projects(request):
+    return render(request, "projects.html")
+
+def blog(request):
+    return render(request, "blog.html")
+

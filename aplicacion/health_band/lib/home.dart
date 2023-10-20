d9264110da91799c5d021200a4756df5d1176eb2
Tobías <paganotobias05@gmail.com>
2023-10-20 15:50:38 -0300

    4 bloques + bienvenida y boton descarga

--- aplicacion/health_band/lib/home.dart
+++ aplicacion/health_band/lib/home.dart
@@ -10,7 +10,7 @@ class HomeView extends StatelessWidget {
         centerTitle: true,
         flexibleSpace: Container(
           width: 393.0,
-          padding: const EdgeInsets.fromLTRB(60.0, 10.0, 60.0, 10.0),
+          padding: const EdgeInsets.fromLTRB(60.0, 50.0, 60.0, 10.0),
           alignment: Alignment.center,
           decoration: const BoxDecoration(
             color: Color.fromARGB(255, 14, 52, 96),
@@ -21,23 +21,49 @@ class HomeView extends StatelessWidget {
       body: Container(
         width: 393.0,
         height: 852.0,
-        padding: const EdgeInsets.fromLTRB(70.0, 25.0, 70.0, 25.0),
+        padding: const EdgeInsets.fromLTRB(30.0, 25.0, 30.0, 25.0),
         child: Column(
           children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             mainAxisSize: MainAxisSize.min,
             children: [
+              Text('Bienvenido, Nombre', style: TextStyle(color: Colors.amber, fontSize: 20.0),),
+              SizedBox(width: 20,),
+              MaterialButton(
+                onPressed: () {}, 
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(5.0, 0, 5.0, 0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset('/Users/tobiaspagano/Documents/GitHub/healthband/aplicacion/health_band/lib/assets/images/info-bBM.png', width: 10.0, height: 10.0,),
+                      Text('Descargar PDF', style: TextStyle(color: Colors.amber, fontSize: 8.0),),
+                    ],
+                  ),
+                ),
+              //AssetImage('/Users/tobiaspagano/Documents/GitHub/healthband/aplicacion/health_band/lib/assets/images/info-bBM.png')
+
+              ),
+            ],
+          ),
+          SizedBox(height: 15.0,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.min,
+            children: [
               Container(
+                padding: EdgeInsets.fromLTRB(4.0, 0.0, 4.0, 0.0),
                 decoration: BoxDecoration(borderRadius: BorderRadius.circular(3.0), color: Colors.grey),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Row(
                     mainAxisAlignment: MainAxisAlignment.start,
                     mainAxisSize: MainAxisSize.min,
                     children: [
                       Image.asset('/Users/tobiaspagano/Documents/GitHub/healthband/aplicacion/health_band/lib/assets/images/pulse-bg.png', width: 5.0, height: 5.0,),
-                      Text('holamundo', style: TextStyle(color: Colors.lightBlue, fontSize: 10)),
+                      Text('Ritmo Cardíaco', style: TextStyle(color: Colors.lightBlue, fontSize: 8)),
                       IconButton(onPressed: (){}, icon: Image.asset('/Users/tobiaspagano/Documents/GitHub/healthband/aplicacion/health_band/lib/assets/images/chevron-Agw.png', width: 3.0, height: 3.0,))
                     ]
                   ),
@@ -54,9 +80,69 @@ class HomeView extends StatelessWidget {
                 ),
               ),
               SizedBox(
-              width: 25.0,
+              width: 10.0,
               ),
               Container(
+                padding: EdgeInsets.fromLTRB(4.0, 0, 4.0, 0),
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(3.0), color: Colors.grey),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                 children: [
+                    Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Image.asset('/Users/tobiaspagano/Documents/GitHub/healthband/aplicacion/health_band/lib/assets/images/pulse-bg.png', width: 6.0, height: 6.0,),
+                      Text('holamundo', style: TextStyle(color: Colors.lightBlue, fontSize: 10)),
+                      IconButton(onPressed: (){}, icon: Image.asset('/Users/tobiaspagano/Documents/GitHub/healthband/aplicacion/health_band/lib/assets/images/chevron-Agw.png', width: 3.0, height: 3.0,))
+                    ]
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text('X', style: TextStyle(color: Colors.amber, fontSize: 25.0),),
+                    ],
+                  ),
+                  ], 
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 15.0,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                padding: EdgeInsets.fromLTRB(5.0, 0.0, 5.0, 0.0),
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(3.0), color: Colors.grey),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Image.asset('/Users/tobiaspagano/Documents/GitHub/healthband/aplicacion/health_band/lib/assets/images/pulse-bg.png', width: 5.0, height: 5.0,),
+                      Text('Ritmo Cardíaco', style: TextStyle(color: Colors.lightBlue, fontSize: 8)),
+                      IconButton(onPressed: (){}, icon: Image.asset('/Users/tobiaspagano/Documents/GitHub/healthband/aplicacion/health_band/lib/assets/images/chevron-Agw.png', width: 3.0, height: 3.0,))
+                    ]
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text('X', style: TextStyle(color: Colors.amber, fontSize: 25.0)),
+                    ],
+                  ),
+                  ],
+                  //decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(2.0)), 
+                ),
+              ),
+              SizedBox(
+              width: 10.0,
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(5.0, 0, 5.0, 0),
                 decoration: BoxDecoration(borderRadius: BorderRadius.circular(3.0), color: Colors.grey),
                 child: Column(
                  children: [
@@ -79,9 +165,6 @@ class HomeView extends StatelessWidget {
                 ),
               ),
             ],
-          ),
-          Row(
-
           ),
           
         ],)

305fb8ba8cc754229c3a3d6d045b1dc147de10ed
gonza-g9673 <gonzalogomez9673@gmail.com>
2023-10-26 13:50:03 -0300

    a
    a

--- Pagina web/test.py
+++ Pagina web/test.py
@@ -1,4 +1,4 @@
 import requests
 info = { 'PULSOS' : 2200 , 'OXIGENO' : 30 , 'TEMPERATURA' : 2 , 'MOVIMIENTO' : "Estoy Ido" }
-g = requests.post("https://www.healthband.com.ar/datos/", json=info)
+g = requests.post("http://192.168.1.111:8080/datos/", json=info)
 print (g.json())

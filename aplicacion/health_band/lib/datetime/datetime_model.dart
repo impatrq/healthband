54018e700e303469db29bb49dd79ad2a2642972a
Tobías <paganotobias05@gmail.com>
2023-11-12 16:10:06 -0300

    nuevos logos hb para iconos

---
+++ aplicacion/health_band/lib/datetime/datetime_model.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class DateTimeDemo extends StatefulWidget {
+  const DateTimeDemo({super.key});
+
+  @override
+  State<DateTimeDemo> createState() => _DateTimeDemoState();
+}
+
+class _DateTimeDemoState extends State<DateTimeDemo> {
+  bool showResult = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}

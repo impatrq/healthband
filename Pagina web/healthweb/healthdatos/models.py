950b56409bafe7da217baa26ed3cd329774736eb
gonza-g9673 <gonzalogomez9673@gmail.com>
2023-11-14 15:35:01 -0300

    BBDD update

--- Pagina web/healthweb/healthdatos/models.py
+++ Pagina web/healthweb/healthdatos/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import datetime 
+from phonenumber_field.modelfields import PhoneNumberField
 
 class Datos(models.Model):
     pulsos = models.FloatField(default=None, null= True)
@@ -7,3 +8,11 @@ class Datos(models.Model):
     temperatura = models.FloatField(default=None, null = True)
     movimiento = models.CharField (max_length=30, null = True)
     time = models.DateTimeField(default=datetime.datetime.now, null=True)
+
+
+class Usuarios(models.Model):
+    nombre = models.CharField(default=None, null=True)
+    telefono = PhoneNumberField(null=False, blank=False, unique=True)
+    email = models.EmailField(default=None, null=True)
+    genero = models.CharField(default=None, null=True)
+    contraseña = models.CharField(default=None, null=True)

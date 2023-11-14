a67490d5fa27f7f146dd831b48c202a4e5c63636
gonza-g9673 <gonzalogomez9673@gmail.com>
2023-11-14 13:08:24 -0300

    Git fix
    se elimina mala clonacion de carpeta pagina web

--- Pagina web/healthweb/healthdatos/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-import datetime
-class Datos(models.Model):
-    pulsos = models.FloatField(default=None, null= True)
-    oxigenacion = models.FloatField(default=None, null= True)
-    temperatura = models.FloatField(default=None, null = True)
-    movimiento = models.CharField (max_length=30, null = True)
-    time = models.DateTimeField(default=datetime.datetime.now, null=True)
-# Create your models here.
